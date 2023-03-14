5b95c137fd359e0da87766fbbf09ed18c17cc018
Micke Nordin <kano@sunet.se>
2023-03-14 16:42:40 +0100

    AdminSettings: Make input boxes wider
    Currently the width for the input boxes resolve to 130px for some
    reason.
    
    This is not very much, so setting the width to auto using the style
    attribute with !important makes it wider.

--- nextcloud-rds/templates/settings-admin.php
+++ nextcloud-rds/templates/settings-admin.php
@@ -12,13 +12,13 @@ script('rds', 'settings-admin');
 
     <form id="rds-settings">
         <p>
-            <input type="text" name="cloudURL" id="cloud_url" class="text" <?php if (!empty($_["cloudURL"])) { ?> value="<?php print_unescaped($_['cloudURL']); ?>" <?php } ?> placeholder="<?php p($l->t('url to rds instance')); ?>" />
+            <input type="text" name="cloudURL" id="cloud_url" class="text" <?php if (!empty($_["cloudURL"])) { ?> value="<?php print_unescaped($_['cloudURL']); ?>" <?php } ?> placeholder="<?php p($l->t('url to rds instance')); ?>" style="width: auto !important" />
             <label for="cloudURL">
                 <?php p($l->t('Specify here the URL, where the ownCloud instance can find your RDS instance e.g. https://url-to-rds-in-k8s. Please leave out the trailing slash.')); ?>
             </label>
         </p>
         <p>
-            <input type="text" name="oauthname" id="oauth_name" class="text" <?php if (!empty($_["oauthname"])) { ?> value="<?php print_unescaped($_['oauthname']); ?>" <?php } ?> placeholder="<?php p($l->t('name of oauth credentials')); ?>" />
+            <input type="text" name="oauthname" id="oauth_name" class="text" <?php if (!empty($_["oauthname"])) { ?> value="<?php print_unescaped($_['oauthname']); ?>" <?php } ?> placeholder="<?php p($l->t('name of oauth credentials')); ?>" style="width: auto !important" />
             <label for="oauthname">
                 <?php p($l->t('Specify here the name of the oauth, which you entered in your ownCloud instance for user oauth authentification. It needs to be the same.')); ?>
             </label>
